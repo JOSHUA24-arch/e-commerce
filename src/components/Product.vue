0c4a4983bd4ebc64d08974d5b5b832c9e6556906
JOSHUA24-arch <mallyajoshua24@gmail.com>
2022-08-19 01:33:39 +0300

    First e commerce

---
+++ src/components/Product.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-row  justify="center" class="space px-16">
+    <v-col cols="12" xs="12" sm="6" md="4" v-for="(product, i) in products" :key="i">
+        <v-card class="mx-auto rounded-x1" max-width="300" flat outlined>
+            <div align="center" justfy="center">
+                <v-img max-height="300" max-width="300" contain :src="product.img"></v-img>
+            </div>
+            <v-card-title>{{product.title}}</v-card-title>
+            <v-card-title class="grey--text text-grey-darken-1 caption mt-n6">{{product.subtitle}}</v-card-title>
+            <v-card-title class="mt-n4">{{product.price}}</v-card-title>
+            <v-card-actions class="mw-2 mt-n4">
+                <v-btn outlined class="mt-n2 add">
+                    <v-icon color="green" @click="decrement">mdi-minus</v-icon>
+                </v-btn>
+
+                <v-spacer></v-spacer>
+                <v-btn class="mx-2 mt-n3" fab dark small color="green">
+                
+                    <v-icon dark>mdi-shopping</v-icon>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        bpm:1,
+        products: [
+            { img: "1.png", title: "Cabbage", subtitle: "1kg", price: "$12"},
+
+            { img: "1.png", title: "Cabbage", subtitle: "1kg", price: "$12"},
+
+            { img: "1.png", title: "Cabbage", subtitle: "1kg", price: "$12"},
+
+            { img: "1.png", title: "Cabbage", subtitle: "1kg", price: "$12"},
+
+            { img: "1.png", title: "Cabbage", subtitle: "1kg", price: "$12"},
+        
+            { img: "1.png", title: "Cabbage", subtitle: "1kg", price: "$12"},
+        ]
+    }),
+
+    methods: {
+        decrement() {
+            this.bpm--
+        },
+        increment() {
+            this.bpm++
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,158 @@
+<template>
+  <v-app>
+    <Navbar />
+
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="mx-auto rounded-xl" flat>
+            <v-img src="1.png" max-height="100%" max-width="100%" contain>
+              <v-card-title class="top ml-10">
+                <h2 class="title1">Order your</h2>
+              </v-card-title>
+
+              <v-card-title class="ml-10 mt-n8">
+                <h2 class="title2">Daily Groceries</h2>
+              </v-card-title>
+
+              <v-card-text class="ml-10">
+                <h3 class="green--text">#Free Delivery</h3>
+              </v-card-text>
+
+              <v-card-text class="ml-10">
+                <v-row>
+                  <v-col cols="4">
+                    <v-text-field
+                      label="Search your grocery here"
+                      class="d-none d-sm-none d-md-flex"
+                      filled
+                      solo
+                      prepend-inner-icon="mdi-magnify"
+                      flat
+                      rounded
+                      outlined
+                      success
+                    >
+                    </v-text-field>
+                  </v-col>
+                  <v-col cols="2" class="marginLeft">
+                    <v-btn
+                      color="green"
+                      rounded
+                      dark
+                      x-large
+                      class="py-7 d-none d-md-flex"
+                      >search</v-btn
+                    >
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-img>
+          </v-card>
+
+          <v-toolbar flat color="transparent">
+            <v-toolbar-title class="text-h6">Category</v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-btn-toggle v-model="toggle_exclusive" group color="#49D9A0">
+              <v-btn>
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-icon>mdi-chevron-right</v-icon>
+            </v-btn-toggle>
+          </v-toolbar>
+
+          <Category />
+
+          <v-toolbar flat color="transparent" class="mt-5">
+            <v-toolbar-title class="text-h6">Populr product</v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-btn rounded color="green" dark class="px-8">See all</v-btn>
+          </v-toolbar>
+
+          <Product />
+          <v-card flat color="#E2F2E5" class="rounded-x1 mt-8">
+            <v-toolbar flat color="transparent" class="mb-4">
+              <v-toolbar-title class="text-h6 mt-5">Popular Bundle pack</v-toolbar-title>
+            </v-toolbar>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import Product from "../components/Product.vue";
+import Category from "../components/Category";
+import Navbar from "../components/Navbar";
+
+export default {
+  name: "Home",
+  data: () => ({
+    toggle_exclusive: 1,
+  }),
+
+  components: {
+    Navbar,
+    Category,
+    Product,
+  },
+};
+</script>
+
+<style>
+.margin-left {
+  margin-left: -90px;
+}
+
+.mtop {
+  margin-top: 100px;
+}
+
+.mbottom {
+  margin-bottom: 100px;
+}
+
+.v-card.borderme {
+  border: 2px solid green !important;
+}
+
+.v-card.borderout {
+  border: 1px solid #d5f0db !important;
+}
+
+.v-btn:not(.v-btn--round).v-size--default.add {
+  min-width: 0px !important;
+}
+
+.theme--light.v--sheet--outlined.mobile {
+  border: 2px solid black !important;
+}
+
+@media only screen and (max-width: 600px) {
+  h2.title1 {
+    font-size: 15px;
+  }
+  h2.title2 {
+    font-size: 15px;
+  }
+
+  .top {
+    margin-top: 20px;
+  }
+}
+
+@media only screen and (min-width: 600px) {
+  .top {
+    margin-top: 70px;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+  .top {
+    margin-top: 120px;
+  }
+}
+</style>
